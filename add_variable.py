e489da0a2309ec6a7a3e4ecc869f5344076d871b
samuelkawuldim <kawuldimsamuel@yahoo.com>
2022-12-15 00:20:33 -0600

    addition,subtraction and multiplication of numbers

---
+++ add_variable.py
@@ -0,0 +1,50 @@
+# store numbers input by the user
+a = input("enter the first number: ")
+b = input("enter second number: ")
+
+# add the numbers
+sum = float(a) + float(b)
+
+# print results
+print("the sum of {0}and{1} is {2}".format(a, b, sum))
+
+# subtraction of numbers
+sub = float(a) - float(b)
+# print results
+print("the subtraction of {0}and {1} is{2}".format(a, b, sub))
+
+# multiplication of numbers
+multiply = float(a)*float(b)
+# print result
+print("the multiplication of {0}and{1} is {2}".format(a, b, multiply))
+
+# addition of numbers
+add = float(a)+float(b)
+# print results
+print("the addition of {0}and{1} is {2}".format(a, b, add))
+
+# division of numbers
+div = float(a)/float(b)
+# print results
+print("the division of {0}and{1} is {2}".format(a, b, div))
+
+
+# addition of numbers
+add = float(a)+float(b)
+# print results
+print("the addition of {0}and{1}is{2}".format(a, b, add))
+
+# division of numbers
+div = float(a)/float(b)
+# print results
+print("the division of {0}and{1}is {2}".format(a, b, div))
+
+# print the addition of numbers
+add = float(a)+float(b)
+# print results
+print("the addition of {0}and{1}is {2}".format(a, b, add))
+
+# print the subtraction of numbers
+sub = float(a)-float(b)
+# print results
+print("the subtraction of {0}and{1}is{2}".format(a, b.sub))
