db22104b0d91ccd50762440f9bc693e3d8453faf
samuelkawuldim <kawuldimsamuel@yahoo.com>
2023-05-24 21:51:50 -0500

    importing modules

---
+++ modules.py
@@ -0,0 +1,23 @@
+# import build in modules in python
+import math
+# import two modules at once
+import math
+#import random
+# import with renaming the modules
+import random as Samuel
+from math import sqrt as Akum
+import os
+# import a specific function from a module
+from math import factorial
+print(math.sqrt(9))
+print(math.log(10))
+print(factorial(4))
+print(math.factorial(5))
+#print(random.randint(23, 37))
+
+# importing power function
+base = 10
+exponent = 8
+print(math.pow(10, 8))
+print(Samuel.randint(10, 30))
+print(Akum(2))
